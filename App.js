d45a35a9ded9e960b05072a395fca5c0e7709496
Luci Pak <pak.luci.13@gmail.com>
2021-04-04 13:20:33 -0700

    got everythingto connect and messages to send, just haven't gotten messages to persist

--- App.js
+++ App.js
@@ -6,8 +6,10 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+const io = require('socket.io-client');
+
 import {
   SafeAreaView,
   ScrollView,
@@ -16,55 +18,65 @@ import {
   Text,
   useColorScheme,
   View,
+  TextInput,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        onPress={() => console.log('hey')}
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         onPress={() => console.log('hey')}
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
 const App = () => {
+  const socket = io('http://localhost:4000');
   const isDarkMode = useColorScheme() === 'dark';
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const handleTest = () => {
-    axios
-      .get('http://localhost:4000/test')
-      .then(() => console.log('successful get'))
-      .catch(err => console.log(err));
+  useEffect(() => {
+    socket.connect();
+    socket.on('connection', () => socket.send('Hello from client!'));
+    socket.on('greetings', greeting => console.log(greeting));
+    socket.on('newMessage', newMessage =>
+      setMessages([...messages, newMessage]),
+    );
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket, messages]);
+
+  const handleChange = async () => {
+    socket.emit('message', text);
+    // setText('Type message here');
   };
 
   return (
@@ -76,9 +88,23 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
           }}>
-          <Text onPress={handleTest} style={styles.textColor}>
-            This is Luci/Erics chat app!
-          </Text>
+          <Text style={styles.textColor}>This is Luci/Erics chat app!</Text>
+          <TextInput
+            placeholder="Test text input!"
+            onChangeText={e => {
+              setText(e);
+            }}
+            value={text}
+          />
+          <Button
+            onPress={handleChange}
+            title="Test Button"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          {messages.map(message => (
+            <Text key={message}>{message}</Text>
+          ))}
         </View>
       </SafeAreaView>
     </ScrollView>
@@ -106,5 +132,28 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 });
+
+// socket.on('connect', () => {
+//   // either with send()
+//   socket.send('Hello!');
+
+//   // or with emit() and custom event names
+//   socket.emit(
+//     'salutations',
+//     'Hello!',
+//     {mr: 'john'},
+//     Uint8Array.from([1, 2, 3, 4]),
+//   );
+// });
+
+// // handle the event sent with socket.send()
+// socket.on('message', data => {
+//   console.log(data);
+// });
+
+// // handle the event sent with socket.emit()
+// socket.on('greetings', (elem1, elem2, elem3) => {
+//   console.log(elem1, elem2, elem3);
+// });
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const port = 4000;
+
 // const axios = require('axios');
 // const path = require('path');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
-app.get('/test', (req, res) => {
-  console.log('reached on server');
-  res.end();
+// app.get('/test', (req, res) => {
+//   console.log('reached on server');
+//   res.send();
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening at http://localhost:${port}`);
+// });
+
+// io.on('connection', () => {
+//   console.log(`socket server listening on localhost: ${port}`);
+// });
+
+// io.on('connection', socket => {
+//   console.log('a user connected :D');
+//   socket.on('chat message', msg => {
+//     console.log(msg);
+//     io.emit('chat message', msg);
+//   });
+// });
+
+io.on('connection', socket => {
+  // either with send()
+  console.log('connection on server to socket io');
+
+  // or with emit() and custom event names
+  socket.emit('greetings', 'hey from server');
+
+  // handle the event sent with socket.send()
+  socket.on('message', data => {
+    socket.emit('newMessage', data);
+  });
+
+  // handle the event sent with socket.emit()
+  socket.on('salutations', (elem1, elem2, elem3) => {
+    console.log(elem1, elem2, elem3);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+server.listen(port, () => console.log('server listening at 4000'));
